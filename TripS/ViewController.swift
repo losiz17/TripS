d67511aaa3f492678664cbc4d5b50d6ebf51b7aa
losiz17 <riokam.11.12@docomo.ne.jp>
2019-01-18 01:38:57 +0900

    Initial Commit

---
+++ TripS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripS
+//
+//  Created by 吉川莉央 on 2019/01/18.
+//  Copyright © 2019 RioYoshikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TripSTests/TripSTests.swift
@@ -0,0 +1,34 @@
+//
+//  TripSTests.swift
+//  TripSTests
+//
+//  Created by 吉川莉央 on 2019/01/18.
+//  Copyright © 2019 RioYoshikawa. All rights reserved.
+//
+
+import XCTest
+@testable import TripS
+
+class TripSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
